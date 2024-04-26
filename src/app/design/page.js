a26193cf606e0a4db4e599a4ada24fb91f39025f
iamrawat21 <90498266+iamrawat21@users.noreply.github.com>
2024-04-26 10:38:05 +0530

    change

--- src/app/design/page.js
+++ src/app/design/page.js
@@ -6,7 +6,7 @@ import Double from './components/double';
 export default function Design() {
   return (
     <main className={styles.main}>
-      <h1>We use design and technology to create brands and products that perform, delight, and scale.</h1>
+      <h1>I Make dope and compelling website</h1>
       <div className={styles.gallery}>
         <Double projects={[projects[0], projects[1]]}/>
         <Double projects={[projects[2], projects[3]]} reversed={true}/>
